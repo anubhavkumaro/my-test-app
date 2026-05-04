d337b009fcaa400b7fa97fe259f732a3b2addc55
anubhavkumaro <anubhavkumar707076@gmail.com>
2026-05-04 12:52:15 +0530

    Project Host

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/', // must match you repo name
+  base:'/my-text-app', // must match you repo name
 })
