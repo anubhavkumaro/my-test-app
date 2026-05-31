65d6aa8777108c6ba327f1426e2c1de8ab6015de
anubhavkumaro <anubhavkumar707076@gmail.com>
2026-05-31 12:13:58 +0530

    Host

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/'
-})
-=======
   base: '/',
 })
->>>>>>> 21ef8c3dbf9ddf31a8dc084bd84311081dbb91e3
+
